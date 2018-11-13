7c7bd411ad67a2b7728a17d25a03cba8bdb8ad5d
Bill Kwai <billkwai.edu@gmail.com>
2018-11-12 19:07:34 -0800

    adds more boilerplate code

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
+#!/usr/bin/env python
 from flask import Flask
 from bs4 import BeautifulSoup
+from scrape import scrape
 
 app = Flask(__name__)
+
+@app.route('/')
+	return 0
+
+if __name__=='__main__':
+	app.run(debug=True)
---
+++ scrape.py
@@ -0,0 +1,5 @@
+def scrape():
+	print "need to implement"
+
+def scrape_fillmore():
+	print "need to implement"
