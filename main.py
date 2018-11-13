93aa45b9d430f43fd99509a3699b1d5dd5cf68ed
Bill Kwai <billkwai.edu@gmail.com>
2018-11-12 19:58:33 -0800

    updates more boilerplate code

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 #!/usr/bin/env python
 from flask import Flask
 from bs4 import BeautifulSoup
+from flask_sqlalchemy import SQLAlchemy
 from scrape import scrape
+import sys
+import json
+from flask_heroku import Heroku
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+heroku = Heroku(app)
+db = SQLAlchemy(app)
 
 app = Flask(__name__)
 
