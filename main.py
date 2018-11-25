de9ce95d5fe1a73a22901cc0309de14b62f14f3d
Bill Kwai <billkwai.edu@gmail.com>
2018-11-24 22:44:51 -0800

    sets up configurations for database

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ import os
 from flask_heroku import Heroku
 
 app = Flask(__name__)
+app.config.from_object(os.environ['APP_SETTINGS'])
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 heroku = Heroku(app)
 db = SQLAlchemy(app)
